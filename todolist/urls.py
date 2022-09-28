9e9c347575e6dd6930bee7daaba71cd6213c82af
Erzete <82033253+Erzete@users.noreply.github.com>
2022-09-28 23:19:42 +0700

    Tugas 4 Submission

--- todolist/urls.py
+++ todolist/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
-    path('add/',add_task,name='add_task')
+    path('create-task/',add_task,name='add_task')
 ]
