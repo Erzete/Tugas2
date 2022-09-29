18fbe2ac51db6615291ae44d2f61fd9b0137444b
Erzete <82033253+Erzete@users.noreply.github.com>
2022-09-29 08:03:26 +0700

    Tugas 4 Fix?

--- project_django/settings.py
+++ project_django/settings.py
@@ -143,3 +143,7 @@ for directory in [*STATICFILES_DIRS, STATIC_ROOT]:
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
